9bf814dcd502e1e82b93e8f0c55fb4dd322e44e1
asalsan790 <asalto@iespuntadelverde.es>
2020-10-02 18:57:08 +0200

    con objeto Vehículo

--- src/index.ts
+++ src/index.ts
@@ -8,21 +8,21 @@ const main = async () => {
     let n: number
     do {
         n = await menuPral()
-        switch(n){
+        switch(n) {
             case 1:
-                console.log("Opción Nuevo Vehículo")
-                marca =  await leerTeclado('Dame la marca') 
-                modelo =  await leerTeclado('Dame el modelo')
+                console.log('Opción Nuevo Vehículo')
+                marca = await leerTeclado('Dame la marca') 
+                modelo = await leerTeclado('Dame el modelo')
                 veh = new Vehiculo(marca, modelo)
                 break
             case 2:
-                console.log("Opción mostrar Vehículo")
+                console.log('Opción mostrar Vehículo')
                 console.log(`La marca es: ${veh.marca}`)
                 console.log(`El modelo es: ${veh.modelo}`)
                 break
             case 3:
-                console.log("Opción cambiar modelo")
-                modelo =  await leerTeclado('Dame el modelo')
+                console.log('Opción cambiar modelo')
+                modelo = await leerTeclado('Dame el modelo')
                 veh.modelo = modelo
                 break                
             case 0:
